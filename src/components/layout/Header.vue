8857fc9593b7715776e15b7ad79cb969c03c0548
study2895 <144806675+study2895@users.noreply.github.com>
2024-10-26 13:38:59 +0900

    [Design] 헤더 디자인 피그마와 조금 더 비슷하게 수정

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- 상단바 -->
   <header
-    class="bg-white shadow-sm py-3 px-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-[395px] min-w-[340px] z-10"
+    class="bg-white shadow-sm py-2 px-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-[395px] min-w-[340px] z-10"
   >
     <div class="flex items-center justify-between">
-      <img src="@/assets/images/Akoming.svg" alt="로고" class="h-8" />
+      <img src="@/assets/images/Akoming.svg" alt="로고" class="h-12" />
       <!-- <button
         class="text-[#F6B87A] hover:bg-[#F6B87A] hover:bg-opacity-10 px-2 py-1 rounded-full transition-colors duration-300 text-sm"
       >
