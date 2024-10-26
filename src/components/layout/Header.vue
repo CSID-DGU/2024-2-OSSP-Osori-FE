28bce61b1274aee24470e94947ef4a78183ab107
study2895 <144806675+study2895@users.noreply.github.com>
2024-10-26 14:23:27 +0900

    [Design] Footer Header 디자인 수정

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 상단바 -->
   <header
-    class="bg-white shadow-sm py-2 px-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-[395px] min-w-[340px] z-10"
+    class="bg-white py-2 px-4 fixed top-0 left-1/2 transform -translate-x-1/2 w-[395px] min-w-[340px] z-10"
   >
     <div class="flex items-center justify-between">
       <img src="@/assets/images/Akoming.svg" alt="로고" class="h-12" />
